87bb260c714483801221a5b9f1fb5f514d11f73f
Wish <100239673+wish13yt@users.noreply.github.com>
2024-12-07 09:20:41 -0600

    modified:   UpE.py

--- UpE.py
+++ UpE.py
@@ -6,10 +6,11 @@ print("MurderBob")
 print("MurderBob Advanced")
 print("LigmaBalls")
 print("UpE")
+print("Banana Simulator")
 program = input("Please select the program you would like to update (CASE-SENSITIVE): ")
 if (program == "MurderBob Advanced"):
     print("Alright! Downloading...")
-    url = 'https://github.com/Freakybob-Team-Games-Games/murderbobadvanced/blob/main/Code/java/Game.java?raw=true'
+    url = 'https://github.com/Freakybob-Team-Games/murderbobadvanced/blob/main/Code/java/Game.java?raw=true'
     response = requests.get(url)
     file_Path = 'MurderBobAdvanced.java'
     if response.status_code == 200:
@@ -30,7 +31,7 @@ if (program == "MurderBob"):
     version = input("What version would you like to download? ")
     if (version == "1"):
         input("WARNING! THIS IS 85.9 MB (shrank in later releases)")
-        url = 'https://github.com/Freakybob-Team-Games-Games/MurderBob/blob/main/Releases/1/MurderBob.jar?raw=true'
+        url = 'https://github.com/Freakybob-Team-Games/MurderBob/blob/main/Releases/1/MurderBob.jar?raw=true'
         response = requests.get(url)
         file_Path = 'MurderBob.jar'
         if response.status_code == 200:
@@ -41,7 +42,7 @@ if (program == "MurderBob"):
         else:
             print('Failed to download file')
     if (version == "2"):
-        url = 'https://github.com/Freakybob-Team-Games-Games/MurderBob/blob/main/Releases/2/MurderBob.jar?raw=true'
+        url = 'https://github.com/Freakybob-Team-Games/MurderBob/blob/main/Releases/2/MurderBob.jar?raw=true'
         response = requests.get(url)
         file_Path = 'MurderBob.jar'
         if response.status_code == 200:
@@ -86,7 +87,7 @@ if (program == "MurderBob"):
             print('Failed to download file')
 if (program == "LigmaBalls"):
     print("Installing to current directory...")
-    url = 'https://github.com/Freakybob-Team-Games/lb/blob/main/lb.py?raw=true'
+    url = 'https://github.com/Freakybob-Team/lb/blob/main/lb.py?raw=true'
     response = requests.get(url)
     file_Path = 'lb.py'
     if response.status_code == 200:
@@ -107,5 +108,29 @@ if (program == "UpE"):
             print('File downloaded successfully')
             file.close()
             exec(open('UpDate.py').read())
+    else:
+        print('Failed to download file')
+if (program == "LigmaBalls"):
+    print("Installing to current directory...")
+    url = 'https://github.com/Freakybob-Team-Games/lb/blob/main/lb.py?raw=true'
+    response = requests.get(url)
+    file_Path = 'lb.py'
+    if response.status_code == 200:
+        with open(file_Path, 'wb') as file:
+            file.write(response.content)
+            print('File downloaded successfully')
+            file.close()
+    else:
+        print('Failed to download file')
+if (program == "Banana Simulator"):
+    print("Installing to current directory...")
+    url = 'https://github.com/Freakybob-Team-Games/Banana-Simulator/blob/main/Banana%20Simulator.py?raw=true'
+    response = requests.get(url)
+    file_Path = 'Banana Simulator.py'
+    if response.status_code == 200:
+        with open(file_Path, 'wb') as file:
+            file.write(response.content)
+            print('File downloaded successfully')
+            file.close()
     else:
         print('Failed to download file')
